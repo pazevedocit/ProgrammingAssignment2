b7a44f66b1fb079ad8ead78cf677841adbe854d3
Paulo Roberto Azevedo <pazevedo@ciandt.com>
2015-06-19 16:27:17 -0300

    Signed-off-by: Paulo Roberto Azevedo <pazevedo@ciandt.com>

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## MakeCacheMatrix
+## This funcitions stores a InvertedMatriz on a cache in the parent scope
 
-## Write a short comment describing this function
+
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setInverted <- function(solve) m <<- solve
+  getInverted <- function() m
+  list(set = set, get = get,
+       setInverted = setInverted,
+       getInverted = getInverted)
 }
 
 
-## Write a short comment describing this function
+## CacheSolve - Invert the matrix only if there isn´t an inverted matrix in the cache
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getInverted()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setInverted(m)
+  m
 }
